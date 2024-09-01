508c9945d0effb2ecb601d00a5e761f58f3116d5
RahulSharma7250 <101405211+RahulSharma7250@users.noreply.github.com>
2024-09-01 14:10:45 +0530

    Initial Commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+    <div className="logo">
+      <h1>Cultural Event</h1>
+    </div>
+    <nav className="nav">
+      <ul>
+        <li><a href="#about">About</a></li>
+        <li><a href="#events">Events</a></li>
+        <li><a href="#gallery">Gallery</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+    <div className="cta">
+      <a href="#tickets" className="cta-button">Get Tickets</a>
+    </div>
+    {/* <div className="social-media">
+      <a href="#facebook"><i className="fab fa-facebook-f"></i></a>
+      <a href="#twitter"><i className="fab fa-twitter"></i></a>
+      <a href="#instagram"><i className="fab fa-instagram"></i></a>
+    </div> */}
+  </header>
+);
+};
+
+export default Header;
+ 
